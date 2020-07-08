a0e96db82f18d787825cd693ddefb692c175911f
Tatsuhiro_mac <mtatsu0323@gmail.com>
2020-07-09 00:23:13 +0900

    松嶋家 甲府駅 住所追加

--- hokkaidotravel/src/components/Nittei.js
+++ hokkaidotravel/src/components/Nittei.js
@@ -53,16 +53,16 @@ export default function Nittei() {
         <div>
             <h1 className="container-title">contents</h1>
             <div className="container">
-                {/* <Date date="日付" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" /> */}
+                {/* <Date date="日付" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" /> */}
                 <Date schedules={schedules.day21} date="7/21" lodgingUrl="https://www.jalan.net/yad316240/?contHideFlg=1&ccnt=yado_reservelist_yado&chkin_date=20200721&chkin_child_num=0&chkin_adult_num=3" lodgingName="ビジネスソーシャルホテル蒲田" spots="観光スポット" />
                 <Date schedules={schedules.day22} date="7/22" lodgingUrl="https://www.jalan.net/yad324568/?contHideFlg=1" lodgingName="湯の川プリンスホテル" spots="観光スポット" />
                 <Date schedules={schedules.day23} date="7/23" lodgingUrl="https://www.jalan.net/yad313982/?contHideFlg=1&ccnt=yado_reservelist_yado&chkin_date=20200723&chkin_child_num=0&chkin_adult_num=3" lodgingName="天然温泉プレミアホテル-CABIN-札幌" spots="観光スポット" />
-                <Date schedules={schedules.day24} date="7/24" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
-                <Date schedules={schedules.day25} date="7/25" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
-                <Date schedules={schedules.day26} date="7/26" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
-                <Date schedules={schedules.day27} date="7/27" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
-                <Date schedules={schedules.day28} date="7/28" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
-                <Date schedules={schedules.day29} date="7/29" lodgingUrl="宿のURL" lodgingName="宿の名前" spots="観光スポット" />
+                <Date schedules={schedules.day24} date="7/24" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" />
+                <Date schedules={schedules.day25} date="7/25" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" />
+                <Date schedules={schedules.day26} date="7/26" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" />
+                <Date schedules={schedules.day27} date="7/27" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" />
+                <Date schedules={schedules.day28} date="7/28" lodgingUrl="https://goo.gl/maps/tfz7XeqgjcGnqNcz7" lodgingName="松嶋家" spots="観光スポット" />
+                <Date schedules={schedules.day29} date="7/29" lodgingUrl="https://goo.gl/maps/Fr87TNZFpQLHqG926" lodgingName="やまなし" spots="観光スポット" />
             </div>
         </div>
     )
