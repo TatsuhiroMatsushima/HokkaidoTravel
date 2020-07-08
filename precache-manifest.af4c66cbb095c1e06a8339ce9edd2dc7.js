b965253fb8e5a2e83aad78ca1c154675b69b56b7
Tatsuhiro_mac <mtatsu0323@gmail.com>
2020-07-08 23:50:13 +0900

    Updates

---
+++ precache-manifest.af4c66cbb095c1e06a8339ce9edd2dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d9aeb7223374439da17e7fa65fcc4c",
+    "url": "/HokkaidoTravel/index.html"
+  },
+  {
+    "revision": "c2a36688ce8f00d51d20",
+    "url": "/HokkaidoTravel/static/css/main.00c322fc.chunk.css"
+  },
+  {
+    "revision": "bdeddcaf04fe967960e1",
+    "url": "/HokkaidoTravel/static/js/2.06c82120.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HokkaidoTravel/static/js/2.06c82120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a36688ce8f00d51d20",
+    "url": "/HokkaidoTravel/static/js/main.fa3be278.chunk.js"
+  },
+  {
+    "revision": "23a25298136285145d8d",
+    "url": "/HokkaidoTravel/static/js/runtime-main.bdbc6cfe.js"
+  }
+]);
