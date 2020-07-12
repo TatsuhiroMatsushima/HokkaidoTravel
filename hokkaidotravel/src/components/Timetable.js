2d26dc492b7c14b9cc0754ca0c08016186cb60ef
Tatsuhiro_mac <mtatsu0323@gmail.com>
2020-07-12 19:48:23 +0900

    レスポンシブ

--- hokkaidotravel/src/components/Timetable.js
+++ hokkaidotravel/src/components/Timetable.js
@@ -14,10 +14,17 @@ export default function Timetable(props) {
                     ))}
                 </tbody>
             </table>
-            <iframe
-                src={props.map}
-                width="50%" height="100%" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0">
-            </iframe>
+            <div className="map">
+                <iframe
+                    src={props.map}
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    allow="fullscreen"
+                    aria-hidden="false"
+                    tabIndex="0">
+                </iframe>
+            </div>
         </div>
     )
 }
